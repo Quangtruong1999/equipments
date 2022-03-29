b8535ea31b9a692d2d6253840d236e07c814865b
= <=>
2022-03-29 17:38:36 +0700

    kế thừa module res_partner

--- set_up_calendar/models/equipment_tag.py
+++ set_up_calendar/models/equipment_tag.py
@@ -5,6 +5,7 @@ class EquipmentTag(models.Model):
     _name = 'equipment.tag'
     _description = 'Equipment tag'
 
+
     name = fields.Char('Tag name', required=True, translate=True)
     description = fields.Text('Mô tả')
 
