333bd65155b33e0ce2535c313cfae2fd66319082
tuanvo1905tav <68835499+tuanvo1905tav@users.noreply.github.com>
2022-03-30 08:54:39 +0700

    Init

--- set_up_calendar/__manifest__.py
+++ set_up_calendar/__manifest__.py
@@ -11,7 +11,6 @@
     'author': "My Company",
     'website': "http://www.yourcompany.com",
 
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
@@ -34,5 +33,5 @@
     'demo': [
         'demo/demo.xml',
     ],
-    'application': True
+    'installable': True,
 }
